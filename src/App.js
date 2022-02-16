09db92ac61c3b16d464609154d881f506aee5c7e
Andrzej Korbiel <akorbiel@online.saba.com>
2022-02-16 23:02:36 +0100

    Add article list filters

--- src/App.js
+++ src/App.js
@@ -5,23 +5,60 @@ import SortingButton from "./components/SortingButton";
 import Filters from "./components/Filters";
 
 const App = () => {
-    const [articles, setArticles] = useState([])
+    const [articles, setArticles] = useState([]);
+    const [filterOptions, updateFilterOptions] = useState([
+        {label: 'fashion', category: 'fashion', checked: true},
+        {label: 'sports', category: 'sport', checked: true}
+    ])
+
+    const formatArticlesData = data => {
+        let formatted = [];
+        data.forEach(el => {
+            formatted = [...formatted, ...el.articles]
+        });
+        setArticles(formatted);
+    }
+
+    const getArticlesData = () => {
+        Promise.all([
+            fetch('http://localhost:6010/articles/sports'),
+            fetch('http://localhost:6010/articles/fashion')
+        ])
+            .then(res => Promise.all(res.map(r => r.json())))
+            .then(data => formatArticlesData(data))
+            .catch(error => console.log(error))
+    }
+
+    const isArticleCategorySelected = prop => {
+        const currentOptionsState = filterOptions.map(el => el);
+        const objectIndex = currentOptionsState.findIndex(el => el.category === prop.category);
+        return currentOptionsState[objectIndex].checked;
+    }
+
+    const checkIsArticleCategorySelected = (article) => {
+        const isSelected = isArticleCategorySelected(article);
+        if (isSelected) {
+            return <SingleArticle data={article} key={article.id} />
+        }
+    }
+
+    const handleChangeFilter = (option) => {
+        const currentOptionsState = filterOptions.map(el => el);
+        const selectedOption = currentOptionsState.filter(el => el.category === option.category);
+        selectedOption[0].checked = !selectedOption[0].checked;
+        updateFilterOptions(currentOptionsState);
+    }
 
     useEffect(() => {
-        fetch('http://localhost:6010/articles/sports')
-            .then(res => res.json())
-            .then(data => setArticles(data.articles))
-            .catch(error => console.log(error))
+        getArticlesData();
     }, [])
 
     return (
         <div>
-            <Filters />
+            <Filters options={filterOptions} action={handleChangeFilter} />
             <SortingButton />
             {articles && articles.length > 0
-                ? articles.map(article => (
-                    <SingleArticle data={article} key={article.id} />
-                ))
+                ? articles.map(article => checkIsArticleCategorySelected(article))
                 : <p>Sorry, error while displaying data. Please refresh the page</p>
             }
         </div>
--- src/components/Filters.js
+++ src/components/Filters.js
@@ -4,6 +4,12 @@ const Filters = props => {
     return (
         <div>
             <p>Data sources</p>
+            {props.options && props.options.map(option => (
+                <div key={option.label}>
+                    <input type="checkbox" id={option.label} name={option.label} checked={option.checked} onChange={() => props.action(option)} />
+                    <label htmlFor={option.label}>{option.label}</label>
+                </div>
+            ))}
         </div>
     )
 }
